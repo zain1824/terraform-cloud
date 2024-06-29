6ceca34adfcb200ddf14f9ee28ebe19fa9b5d575
zain1824 <56321860+zain1824@users.noreply.github.com>
2024-06-29 15:17:55 -0600

    added data source

---
+++ rds/main.tf
@@ -0,0 +1,15 @@
+data "terraform_remote_state" "vpc" {
+  backend = "remote"
+  config = {
+    organization = "zain1824"
+    workspaces = {
+      name = "vpc"
+    }
+  }
+}
+
+
+resource "aws_db_subnet_group" "cloud" {
+  name       = "cloud"
+  subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnets
+}
